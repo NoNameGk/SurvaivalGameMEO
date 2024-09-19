b042340594a22982c53ef5d5d1ba53549b8864d6
NoNameGk <133035466+NoNameGk@users.noreply.github.com>
2024-09-19 21:36:33 +0400

    first commit
    My first commit

---
+++ GitTestProgect/Assets/Scripts/GitTestScripts.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTestScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // It's my first test script in system "Git" and "GitHub"
+}
